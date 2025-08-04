e5fad0b3a428dccd6d68c89b770d9b587dfb14e8
Jenny <jenny.m.lewis@hotmail.co.uk>
2025-08-04 09:35:06 +0100

    Intro text

--- src/pages/CareerPathTest/CareerPathTest.jsx
+++ src/pages/CareerPathTest/CareerPathTest.jsx
@@ -51,6 +51,18 @@ export default function CareerPathTest() {
           />
         ))}
       </section>
+
+      <section className="careerPathTest--intro">
+        <p>
+          We've analysed data from thousands of our members who work in graduate
+          roles across a range of sectors to understand which personalities,
+          skills and values best fit each career path.
+        </p>
+        <p>
+          Take this test to understand what career path you might be suited to
+          and how to get started.
+        </p>
+      </section>
     </>
   );
 }
