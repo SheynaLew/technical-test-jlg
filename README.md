91dd070dbcb13f245b7439d3c9a4753dc993bfb3
Jenny <jenny.m.lewis@hotmail.co.uk>
2025-08-04 10:53:24 +0100

    QuestionCard styling

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## With More Time
 
 - I would make the InfoCards more responsive and work on accessiblity. At the moment, they are not responsive to mobile screens. I would reduce to font-size, make the cards and icons slightly smaller, and consider stacking them vertically (however, I feel that this would detract from the main content of the page, which should be the test, so I would lean towards removing the cards entirely rather than stacking them).
+- I would have a styling variables file so that things like color are easily set and changed without the need to find every instance of the colour in the app.
+- I would consider accessibilty: The colour of the questions number is not accessible at the moment and could cause issues
+- I would set font size in the index.css, rather than individually across multiple css files.
--- src/templates/QuestionsCard/QuestionCard.jsx
+++ src/templates/QuestionsCard/QuestionCard.jsx
@@ -1,19 +1,23 @@
+import "./QuestionCard.css";
+
 export default function QuestionCard({ progress, questions }) {
   return (
     <section className="questionsCard--container">
       <p className="questionsCard--progress">Your progress - {progress}%</p>
       {/* ProgressBar component here */}
       <div className="questionsCard--question-container">
-        <p className="questionsCard--question">
-          <span className="questionsCard--question-number">
-            Q1/{questions.length}{" "}
-          </span>
-          {/* Once I have the submission data I can set the question number correctly. Hardcoded for now. */}
-          In a job, I would be motivated by:
-        </p>
-        <p className="questionsCard--question-text">{questions[0].text}</p>
+        <span className="questionsCard--question-number">
+          Q1/{questions.length}{" "}
+        </span>
+        <div className="questionsCard--question">
+          <p className="questionsCard--question-prompt">
+            {/* Once I have the submission data I can set the question number correctly. Hardcoded for now. */}
+            In a job, I would be motivated by:
+          </p>
+          <p className="questionsCard--question-text">{questions[0].text}</p>
 
-        {/* OpinionBar component here    */}
+          {/* OpinionBar component here    */}
+        </div>
       </div>
       <p className="questionsCard--question-review-text">
         To review your previous answers, scroll back before selecting "finish".
